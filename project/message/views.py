1e9448e86a4648b5f096d17b290004497b0f6381
JIWOOSUNG <woosung727@naver.com>
2023-07-05 17:14:37 +0900

    편지생성

--- project/message/views.py
+++ project/message/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import LetterSerializer
+from accounts.models import User
 
-# Create your views here.
+@api_view(['POST'])
+def create_letter(request):
+    user_email = request.data.get('user_email')
+    content = request.data.get('content')
+
+    try:
+        user = User.objects.get(email=user_email)
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=400)
+
+    data = {'user': user.id, 'content': content}
+    serializer = LetterSerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
