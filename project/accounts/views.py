27910b9437d8eef52f49592be610f4b573a63ebd
DrHagha <coby4313@gmail.com>
2023-07-08 00:10:24 +0900

    임시 킵

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -10,7 +10,6 @@ from allauth.socialaccount.models import SocialAccount
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.providers.google import views as google_view
-import json
 
 from rest_framework.views import APIView
 from accounts.serializers import *
@@ -23,65 +22,18 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from .serializers import UserInfoSerializer
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from accounts.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.exceptions import NotFound
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.views.decorators.csrf import csrf_exempt
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # 구글 소셜로그인 변수 설정
 state = os.environ.get("STATE")
 BASE_URL = 'http://localhost:8000/'
-GOOGLE_CALLBACK_URI = BASE_URL + 'api/accounts/google/callback/'
-
-
-
-def get_user_email(key):
-
-    try:
-        token_user = Token.objects.get(key=key)
-        user = User.objects.get(id=token_user.user_id)
-        user_data = UserInfoSerializer(user)
-        print(user_data)
-        return user
-    except :
-        print("error")
-    
+# GOOGLE_CALLBACK_URI = BASE_URL + 'api/accounts/google/callback/'
+GOOGLE_CALLBACK_URI = 'http://localhost:3000'
 
 def google_login(request):
     scope = "https://www.googleapis.com/auth/userinfo.email"
     client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
     return redirect(f"https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&response_type=code&redirect_uri={GOOGLE_CALLBACK_URI}&scope={scope}")
-
-@csrf_exempt
-def update_nickname(request):
-    if request.method == 'PUT':
-        key = request.META.get('HTTP_KEY')  # 헤더에서 key 값을 가져옵니다.
-        reqesut_data = request.body.decode('utf-8')  # 닉네임 값을 요청의 본문에서 가져옵니다.
-        data_json = json.loads(request.body)
-        
-        try:
-            user = get_user_email(key=key)  # key에 해당하는 사용자를 찾습니다.
-            print("==================================")
-            print(type(data_json))
-            print(data_json)
-            print(data_json["nickname"])
-            print("==================================")
-            
-            user.nickname = data_json["nickname"]  # 닉네임을 업데이트합니다.
-            user.save()  # 변경 사항을 저장합니다.
-            
-            response_json = {'email' : user.email, 'key' : key, 'nickname' : user.nickname, 'user_id' : user.id}
-            return JsonResponse(response_json)
-        except User.DoesNotExist:
-            return JsonResponse({'message': 'User not found.'}, status=404)
-    else:
-        return JsonResponse({'message': 'Invalid request method.'}, status=400)
 
 def google_callback(request):
     client_id = os.environ.get("SOCIAL_AUTH_GOOGLE_CLIENT_ID")
@@ -94,7 +46,7 @@ def google_callback(request):
     ### 1-1. json으로 변환 & 에러 부분 파싱
     token_req_json = token_req.json()
     error = token_req_json.get("error")
-
+    
     ### 1-2. 에러 발생 시 종료
     if error is not None:
         raise JSONDecodeError(error)
@@ -115,7 +67,6 @@ def google_callback(request):
     ### 2-2. 성공 시 이메일 가져오기
     email_req_json = email_req.json()
     email = email_req_json.get('email')
-    user_id = email_req_json.get('user_id') 
 
     # return JsonResponse({'access': access_token, 'email':email})
 
@@ -125,9 +76,6 @@ def google_callback(request):
     try:
         # 전달받은 이메일로 등록된 유저가 있는지 탐색
         user = User.objects.get(email=email)
-        email = user.email
-        nickname = user.nickname
-        db_user_id = user.id
 
         # FK로 연결되어 있는 socialaccount 테이블에서 해당 이메일의 유저가 있는지 확인
         social_user = SocialAccount.objects.get(user=user)
@@ -147,15 +95,11 @@ def google_callback(request):
 
         accept_json = accept.json()
         accept_json.pop('user', None)
-        key = accept_json["key"]
-
-        # JsonResponse에 email과 user_id 값을 추가하여 반환
-        return JsonResponse({'key': key, 'email': email, 'user_id': db_user_id, "user_nickname" : nickname})
+        return JsonResponse(accept_json)
 
     except User.DoesNotExist:
         # 전달받은 이메일로 기존에 가입된 유저가 아예 없으면 => 새로 회원가입 & 해당 유저의 jwt 발급
         data = {'access_token': access_token, 'code': code}
-
         accept = requests.post(f"{BASE_URL}api/accounts/google/login/finish/", data=data)
         accept_status = accept.status_code
 
@@ -176,6 +120,7 @@ class GoogleLogin(SocialLoginView):
     callback_url = GOOGLE_CALLBACK_URI
     client_class = OAuth2Client
     
+####
 
 class RegisterAPIView(APIView):
     def post(self, request):
@@ -279,5 +224,8 @@ class AuthAPIView(APIView):
         response.delete_cookie("access")
         response.delete_cookie("refresh")
         return response
-
     
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
