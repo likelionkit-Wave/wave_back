e5f100cca2624d59cdb498cf8f2791e72d2bd43a
DrHagha <coby4313@gmail.com>
2023-07-07 23:01:38 +0900

    db 일부 수정

--- project/message/urls.py
+++ project/message/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
 from message import views
-from message.views import LetterAPIView
 
 urlpatterns = [
     path('create/', views.create_letter, name='create_letter'),
