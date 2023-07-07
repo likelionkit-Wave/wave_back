b0449ee94ff974f90d26baee121ded5e5b735bec
DrHagha <coby4313@gmail.com>
2023-07-07 22:34:06 +0900

    메세지 병합 중

--- project/message/serializers.py
+++ project/message/serializers.py
@@ -4,4 +4,5 @@ from .models import Letter
 class LetterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Letter
+        
         fields = ['id', 'title','user', 'content', 'is_read', 'writer','send_date',]
