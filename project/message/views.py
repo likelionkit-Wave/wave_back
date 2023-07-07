affdc23d562b891c57383d006aa6c105e236a282
김민준 <minduun@gimminjun-ui-MacBookAir.local>
2023-07-07 22:18:31 +0900

    충돌 해결

--- project/message/views.py
+++ project/message/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import LetterSerializer
 from accounts.models import User
+from .models import Letter
+from rest_framework.views import APIView
 
 @api_view(['POST'])
 def create_letter(request):
@@ -20,3 +22,17 @@ def create_letter(request):
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
+
+class LetterAPIView(APIView):
+    model = Letter
+    def get(self, request):
+        letters = Letter.objects.all()
+        serializer = LetterSerializer(letters, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = LetterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
