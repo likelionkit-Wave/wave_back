c80da061a0d4f4c3ff8e4d425dea3106d135a7ea
김민준 <minduun@gimminjun-ui-MacBookAir.local>
2023-07-07 12:35:19 +0900

    Letter API

--- project/config/urls.py
+++ project/config/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from message import views
+from message.views import LetterAPIView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('allauth.urls')),
     path('api/accounts/', include('accounts.urls')),
     path('api/message/', include('message.urls')),
+    path('api/Letter_list/', LetterAPIView.as_view(), name = 'Letter_list'),
 ]
